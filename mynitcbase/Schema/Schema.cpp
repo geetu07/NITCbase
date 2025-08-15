9ffcda02eaa1aa930b42976aaf76943113fdd1fe
geetu07 <kothageethika07@gmail.com>
2025-08-15 23:18:10 +0530

    Stage 5 complete

--- mynitcbase/Schema/Schema.cpp
+++ mynitcbase/Schema/Schema.cpp
@@ -1,4 +1,33 @@
 #include "Schema.h"
-
 #include <cmath>
 #include <cstring>
+
+int Schema::openRel(char relName[ATTR_SIZE]) {
+  int ret = OpenRelTable::openRel(relName);
+
+  // the OpenRelTable::openRel() function returns the rel-id if successful
+  // a valid rel-id will be within the range 0 <= relId < MAX_OPEN and any
+  // error codes will be negative
+  if(ret >= 0){
+    return SUCCESS;
+  }
+
+  //otherwise it returns an error message
+  return ret;
+}
+
+int Schema::closeRel(char relName[ATTR_SIZE]) {
+  if (strcmp(relName,RELCAT_RELNAME)==0 || strcmp(relName,ATTRCAT_ATTR_RELNAME)==0) {/* relation is relation catalog or attribute catalog */
+    return E_NOTPERMITTED;
+  }
+
+  // this function returns the rel-id of a relation if it is open or
+  // E_RELNOTOPEN if it is not. we will implement this later.
+  int relId = OpenRelTable::getRelId(relName);
+
+  if (relId==E_RELNOTOPEN) {/* relation is not open */
+    return E_RELNOTOPEN;
+  }
+
+  return OpenRelTable::closeRel(relId);
+}
